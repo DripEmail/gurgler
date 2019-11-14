f467d4a683d7174b2850e812614f1956aa880e14
Matt Josse <matt.josse@drip.com>
2019-11-14 11:21:45 -0700

    add known working content-type headers

--- bin/utils.js
+++ bin/utils.js
@@ -6,16 +6,20 @@ const getContentType = (ext) => {
   if  (ext === ".css") {
     contentType = "text/css";
   } else if (ext === ".json") {
-    contentType = "application/json"
+    contentType = "application/json";
   } else if (ext === ".html") {
-    contentType = "text/html"
-  } else if (ext ===  ".js") {
-    contentType = "application/javascript"
+    contentType = "text/html";
+  } else if (ext === ".js") {
+    contentType = "application/javascript";
+  } else if (ext === ".svg") {
+    contentType = "image/svg+xml";
+  } else if (ext === ".woff") {
+    contentType = "application/font-woff";
   }
   return contentType;
-}
+};
 
 module.exports = {
   shortHash,
   getContentType
-}
+};
