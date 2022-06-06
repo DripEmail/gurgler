a49559637bc941c3bd4529ea178ca1ec82e9b107
Jachin Rupe <jachin.rupe@drip.com>
2022-06-06 12:30:40 -0500

    Adding npmignore file and removing gurgler.js

--- gurgler.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const publicPath = () => {
-  const data = fs.readFileSync("gurgler.json");
-  return JSON.parse(data).prefix + "/";
-}
-
-module.exports.publicPath = publicPath;
