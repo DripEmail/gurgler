fc99fdaf8ae75bf26df319b00a34f17b7275ce14
Jachin Rupe <jachin.rupe@drip.com>
2022-08-02 17:06:02 -0500

    If a commit is missing, don't just fall over but just show a lot of `?`

--- bin/git.js
+++ bin/git.js
@@ -8,8 +8,11 @@ const getCommitAuthor = (gitSha) => {
   return new Promise((resolve, reject) => {
     exec(`git show --pretty='%aN' --no-patch ${gitSha}`, (err, stdout) => {
       if (err) {
-        console.error(`exec error: ${err}`);
-        reject();
+        if (err.message.includes("unknown revision or path not in the working tree")) {
+          reject(new Error(`Unknown commit hash when trying to look up the author: ${gitSha}`));
+        } else {
+          reject(err);
+        }
       }
       resolve(stdout.trim());
     })
@@ -24,8 +27,11 @@ const getBranch = (gitSha) => {
   return new Promise((resolve, reject) => {
     exec(`git rev-parse --abbrev-ref ${gitSha}`, (err, stdout) => {
       if (err) {
-        console.error(`exec error: ${err}`);
-        reject();
+        if (err.message.includes("unknown revision or path not in the working tree")) {
+          reject(new Error(`Unknown commit hash when trying to look up the branch: ${gitSha}`));
+        } else {
+          reject(err);
+        }
       }
       resolve(stdout.trim());
     })
@@ -40,8 +46,11 @@ const getCommitMessage = (gitSha) => {
   return new Promise((resolve, reject) => {
     exec(`git show --pretty='%s' --no-patch ${gitSha}`, (err, stdout) => {
       if (err) {
-        console.error(`exec error: ${err}`);
-        reject();
+        if (err.message.includes("unknown revision or path not in the working tree")) {
+          reject(new Error(`Unknown commit hash when trying to look the commit message: ${gitSha}`));
+        } else {
+          reject(err);
+        }
       }
       resolve(stdout.trim());
     })
@@ -56,14 +65,15 @@ const getCommitDate = (gitSha) => {
   return new Promise((resolve, reject) => {
     exec(`git show --pretty='%ah' --no-patch ${gitSha}`, (err, stdout, stderr) => {
       if (err) {
-        console.error(`exec error: ${err}`);
-        reject();
+        if (err.message.includes("unknown revision or path not in the working tree")) {
+          reject(new Error(`Unknown commit hash when trying to look up the date of the commit: ${gitSha}`));
+        } else {
+          reject(err);
+        }
       }
-
       if (stderr) {
         console.error(`exec error getCommitDate: ${stderr}`);
       }
-
       resolve(stdout.trim());
     })
   })
@@ -75,23 +85,33 @@ const getCommitDate = (gitSha) => {
  * @returns {Promise<Map<string, string>>}
  */
 const getGitInfo = async (gitSha) => {
-  return await Promise.all(
+  return await Promise.allSettled(
     [getCommitAuthor(gitSha),
       getBranch(gitSha),
       getCommitMessage(gitSha),
       getCommitDate(gitSha)
     ]).then(values => {
-    const gitInfo = new Map();
-    const [author, branch, message, date] = values;
+      const gitInfo = new Map();
 
-    gitInfo.set("gitSha", gitSha);
-    gitInfo.set("author", author);
-    gitInfo.set("branch", branch);
-    gitInfo.set("message", message);
-    gitInfo.set("date", date);
+      if (values.some(v => v.status === "rejected")) {
+        gitInfo.set("gitSha", gitSha);
+        gitInfo.set("author", "????");
+        gitInfo.set("branch", "????");
+        gitInfo.set("message", "????");
+        gitInfo.set("date", "????");
 
-    return gitInfo;
-  });
+        return gitInfo;
+      } else  {
+        const [authorResult, branchResult, messageResult, dateResult] = values;
+        gitInfo.set("gitSha", gitSha);
+        gitInfo.set("author", authorResult.value);
+        gitInfo.set("branch", branchResult.value);
+        gitInfo.set("message", messageResult.value);
+        gitInfo.set("date", dateResult.value);
+
+        return gitInfo;
+      }
+    });
 }
 
 module.exports = {
